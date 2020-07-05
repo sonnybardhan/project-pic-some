35de8ba7de3267f84cf77c7990772d0a43a292e0
sonnybardhan <sonnybardhan@github.io>
2020-07-05 22:27:11 +0530

    fetch images

--- src/Context.js
+++ src/Context.js
@@ -1,8 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 const { Provider, Consumer } = React.createContext();
 
 function ContextProvider(props) {
-	return <Provider value={''}>{props.children}</Provider>;
+	const [ photos, setPhotos ] = useState([]);
+
+	useEffect(() => {
+		fetch('https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json')
+			.then((res) => res.json())
+			.then(setPhotos);
+	}, []);
+	console.log(photos);
+
+	return <Provider value={{ photos }}>{props.children}</Provider>;
 }
 
 export { ContextProvider, Consumer as ContextConsumer };
