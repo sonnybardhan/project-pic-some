256268ac1faea86cf195f9341bda36c90c6625d1
sonnybardhan <sonnybardhan@github.io>
2020-07-06 01:57:51 +0530

    fix image hover issue, implement toggleFavorite method

--- src/Context.js
+++ src/Context.js
@@ -4,14 +4,23 @@ const Context = React.createContext();
 function ContextProvider(props) {
 	const [ photos, setPhotos ] = useState([]);
 
+	const toggleIsFavorite = (id) => {
+		const updatedPhotos = photos.map((photo) => {
+			if (photo.id === id) {
+				return { ...photo, isFavorite: !photo.isFavorite };
+			}
+			return photo;
+		});
+		setPhotos(updatedPhotos);
+	};
+
 	useEffect(() => {
 		fetch('https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json')
 			.then((res) => res.json())
 			.then((data) => setPhotos(data));
 	}, []);
 
-	// return <Provider value={{ photos }}>{props.children}</Provider>;
-	return <Context.Provider value={{ photos }}>{props.children}</Context.Provider>;
+	return <Context.Provider value={{ photos, toggleIsFavorite }}>{props.children}</Context.Provider>;
 }
 
 export { ContextProvider, Context };
--- src/components/Image.js
+++ src/components/Image.js
@@ -1,6 +1,8 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, Fragment, useContext } from 'react';
+import { Context } from '../Context';
 
 export default function Image({ className, img }) {
+	const { toggleIsFavorite } = useContext(Context);
 	const [ hovered, setHovered ] = useState(false);
 
 	const onHover = () => {
@@ -10,20 +12,23 @@ export default function Image({ className, img }) {
 		setHovered(false);
 	};
 
-	const displayIcons = () => {
-		return hovered ? (
-			<Fragment>
-				<i className="ri-heart-line favorite" /> <i className="ri-add-circle-line cart" />
-			</Fragment>
-		) : (
-			''
-		);
+	const handleToggleFavorite = () => {
+		toggleIsFavorite(img.id);
 	};
 
+	const heartIcon = img.isFavorite ? (
+		<i className="ri-heart-fill favorite" onClick={handleToggleFavorite} />
+	) : (
+		hovered && <i className="ri-heart-line favorite" onClick={handleToggleFavorite} />
+	);
+
+	const plusIcon = hovered && <i className="ri-add-circle-line cart" />;
+
 	return (
-		<div className={`${className} image-container`}>
-			{displayIcons()}
-			<img src={img.url} alt="" className="image-grid" onMouseEnter={onHover} onMouseLeave={onLeave} />
+		<div className={`${className} image-container`} onMouseEnter={onHover} onMouseLeave={onLeave}>
+			{heartIcon}
+			{plusIcon}
+			<img src={img.url} alt="" className="image-grid" />
 		</div>
 	);
 }
